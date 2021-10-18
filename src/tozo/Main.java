532385bfec105c49bf2130625b061d0ea042c57e
CHerron42 <connie.lds@herrons.com>
2021-10-18 11:30:15 -0400

    Connie added 1 line

--- src/tozo/Main.java
+++ src/tozo/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kokou.");
         System.out.println("Hello, my name is Tiempo");
-
-
+        System.out.println("Hello,my name is Connie");
     }
 }
